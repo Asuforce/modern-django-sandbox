5bef9372e7724db0130ec7b07fb294a274806727
asuforce <owata.sn@gmail.com>
2018-11-14 16:09:49 +0900

    Set routes

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,3 +1,10 @@
 from django.conf.urls import url, include
+from rest_framework import routers
+from project.api import views
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
